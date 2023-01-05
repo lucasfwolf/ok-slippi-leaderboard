8867d4c95f710f130e735b064461269bc99c4141
Lucas Wolf <lucas.fr.wolf@gmail.com>
2023-01-04 22:19:06 -0600

    almost there

--- cron/run.sh
+++ cron/run.sh
@@ -2,4 +2,4 @@
 PATH="/home/lucasfwolf/.vscode-server/bin/e8a3071ea4344d9d48ef8a4df2c097372b0c5161/bin/remote-cli:/home/lucasfwolf/.nvm/versions/node/v18.12.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program"
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.." 
-node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+"/home/lucasfwolf/.nvm/versions/node/v18.12.0/bin/node" --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
